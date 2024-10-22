8d3671fb1a15fe284546302dc6fb6f232796c9fd
CarlitoJuarez <carlito.b.juarez@gmail.com>
2024-10-22 11:21:07 +0200

    commented the comments ;)

--- srcs/exit_clean.c
+++ srcs/exit_clean.c
@@ -1,6 +1,30 @@
 #include "../minishell.h"
 
-// overwritten with checks and the ex flag is for if it should exit or not
+// overwritten with checks and the ex flag is for if it should exit or not // but it exits anyway in the end ?? 
+// but good idea anyways, maybe this version is better.. 
+// void	error_code(t_pipex *data, char *line, int ex)
+// {
+// 	if (ex)
+// 	{
+// 		if (data && line)
+// 			free_a(line, data);
+// 		else if (line)
+// 			free(line);
+// 		else if (data)
+// 			free_struct(data);
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	else
+// 	{
+// 		if (data && line)
+// 			free_a(line, data);
+// 		else if (line)
+// 			free(line);
+// 		else if (data)
+// 			free_struct(data);
+// 	}
+// }
+
 void	error_code(t_pipex *data, char *line, int ex)
 {
 	if (!data && !ex)
